2c580446f2b8210a45db56de25e1dd2e433bf7c3
sawankshrma <thesawan47@gmail.com>
2025-06-28 02:47:42 +0530

    change models for having a newest first comments and bids on product page

--- auctions/models.py
+++ auctions/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class User(AbstractUser):
@@ -37,6 +38,10 @@ class Bid(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bids')
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-created_at']
+ 
+
     def __str__(self):
         return f"{self.amount} from user: ({self.user.username}) @ {self.listing.title}"
 
@@ -45,6 +50,11 @@ class Comment(models.Model):
     text = models.TextField(max_length=300)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='comments')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
 
     def __str__(self):
         return f"Comment by {self.user.username} on {self.listing.title}"
